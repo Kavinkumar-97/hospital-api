411d2af57f1806a74309c290e666d544f9f3fe98
Kavinkumar-97 <56212561+Kavinkumar-97@users.noreply.github.com>
2023-05-03 17:00:44 +0530

    ae

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const db = require('./config/mongoose');
 const passport = require('passport');
 const passportJwt = require('./config/passport_jwt_strategy');
 
-// Configure middle to parse json
+// Configure middleware to parse json request
 app.use(express.json());
 
+// Initialize passport
 app.use(passportJwt.initialize());
 
+// Configuring routes
 app.use('/', require('./routes'));
 
+// Listening to port
 app.listen(PORT, function (err) {
   if (err) {
     return console.error(err);
   }
 
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
